01ce843a15d8dd7755ace5d2b111883d40c2140f
zhuk <charizardik64@gmail.com>
2022-07-21 16:21:26 +0300

    added reviews(slider)

--- main-page/js/script.js
+++ main-page/js/script.js
@@ -6,10 +6,12 @@ if (swiperReviews) {
     const swiper = new Swiper('.swiper-reviews', {
         // Optional parameters
         loop: true,
+        autoHeight: true,
 
         // If we need pagination
         pagination: {
             el: '.swiper-pagination',
+            clickable: true,
         },
     })
 }
